3f30a9c60b5fe25625ffa9f945f618556d611fdf
Leonardo Rodrigues <leorodriguesrj@gmail.com>
2022-03-19 18:36:42 -0300

    mudamos a pergunta para portugues

--- experiments/hello-world.js
+++ experiments/hello-world.js
@@ -3,6 +3,6 @@ console.log("hello world");
 
 const readline = require('readline-sync');
 
-let name = readline.question("What is your name?");
+let name = readline.question("Qual seu nome?");
 
 console.log("Hi " + name + ", nice to meet you.");
