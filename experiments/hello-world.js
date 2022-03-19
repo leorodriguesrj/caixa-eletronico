4c00c35a7aa4f3adab679df76c43cb2601c7b137
Leonardo Rodrigues <leorodriguesrj@gmail.com>
2022-03-19 18:30:06 -0300

    primeiro commit

---
+++ experiments/hello-world.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+
+
+const readline = require('readline-sync');
+
+let name = readline.question("What is your name?");
+
+console.log("Hi " + name + ", nice to meet you.");
---
+++ experiments/soma.js
@@ -0,0 +1,7 @@
+const soma = 5 + 2;
+
+console.log(soma)
+
+soma = 3 + 7;
+
+console.log(soma)
