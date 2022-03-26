23e90e569f231a3c18d6684802f867a41237db06
Tassio Martins <tassiomrtns@gmail.com>
2022-03-26 17:51:04 -0300

    refatoracao do calculo de notas de 20

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ console.log("Bem vindo ao Banco Hsbc, as notas disponiveis sao: 20, 50, 100.");
 
 let valor = readline.question("Quanto gostaria de sacar ?");
 
-let notas20 = valor / 20;
+let resto = valor % 20;
 
-let resto =  valor % 20;
-
-if (resto === 0)
+if (resto === 0) {
+    let notas20 = valor / 20;
     console.log(`Notas de 20: ${notas20} `);
-else
+} else
     console.log("O valor inserido e invalido, por favor, insira numeros multiplos de 20.");
