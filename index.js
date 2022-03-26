e58667166b56082307e5c1ca4fc3b864b47cdf1c
Tassio Martins <tassiomrtns@gmail.com>
2022-03-26 17:04:52 -0300

    calculamos as qtns de notas de 20
    obs: ainda nao lidamos com fracoes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-console.log("Bem vindo ao Banco Hsbc, as notas disponiveis sao: 20, 50, 100.");
-
 const readline = require("readline-sync");
 
+console.log("Bem vindo ao Banco Hsbc, as notas disponiveis sao: 20, 50, 100.");
+
 let valor = readline.question("Quanto gostaria de sacar ?");
+
+let notas20 = valor / 20;
+
+console.log(`Notas de 20: ${notas20} `);
