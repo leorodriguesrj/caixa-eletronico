018be789281f0a242800ce36e9bd60cd638eccc5
Tassio Martins <tassiomrtns@gmail.com>
2022-03-26 16:15:07 -0300

    usuario informa o valor de saque
    obs: ainda nao faz calculo

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Bem vindo ao Banco Hsbc, as notas disponiveis sao: 20, 50, 100.");
+
+const readline = require("readline-sync");
+
+let valor = readline.question("Quanto gostaria de sacar ?");
